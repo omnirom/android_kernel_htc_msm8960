d3bf3181d6edf8e8da44184c5da45784a7c2b5d8
Jon Haus <haus.xda@gmail.com>
2013-12-30 06:15:36 -0500

    change entropysel back to longentropysel until userspace is updated
    Change-Id: If7220c4846a2565516f5c8b4ff357a729995fa75

--- include/linux/msm_vidc_enc.h
+++ include/linux/msm_vidc_enc.h
@@ -607,7 +607,8 @@ struct venc_capability{
 };
 
 struct venc_entropycfg{
-	unsigned long	entropysel;
+	unsigned long   longentropysel;
+//	unsigned long	entropysel;
 	unsigned long	cabacmodel;
 };
 
